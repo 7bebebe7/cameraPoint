f8f9b9174864488b1ea56523c2b40eed070c187d
7bebebe7 <oleh.sobchenko@gmail.com>
2026-09-03 08:37:56 +0300

    Button fix

--- scripts/main.js
+++ scripts/main.js
@@ -10,41 +10,35 @@ Events.on(ClientLoadEvent, e => {
     //настройка позиции кнопки для удобной игры
     t.add("[gray]Button Position").row()
     
-    t.check("Top", Core.settings.getBool("BPT", false), v => {
-      Core.settings.put("BPT", true)
-      if (Core.settings.getBool("BPT")) {
-        Core.settings.put("BPR", false)
-        Core.settings.put("BPB", false)
-        Core.settings.put("BPL", false)
-      }
+    t.check("Right", Core.settings.getBool("BPR", false), v => {
+        Core.settings.put("BPR", v)
+    
+        if(v){
+            Core.settings.put("BPT", false)
+            Core.settings.put("BPB", false)
+            Core.settings.put("BPL", false)
+        }
     }).left().center().row()
     
-    t.check("Right", Core.settings.getBool("BPR", false), v => {
-      Core.settings.put("BPR", true)
-      if (Core.settings.getBool("BPR")) {
-        Core.settings.put("BPT", false)
-        Core.settings.put("BPB", false)
-        Core.settings.put("BPL", false)
-      }
-    }).left().row()
-    
     t.check("Bottom", Core.settings.getBool("BPB", true), v => {
-      Core.settings.put("BPB", true)
-      if (Core.settings.getBool("BPB")) {
-        Core.settings.put("BPR", false)
-        Core.settings.put("BPT", false)
-        Core.settings.put("BPL", false)
-      }
-    }).left().row()
+        Core.settings.put("BPB", v)
     
-    t.check("Left", Core.settings.getBool("BPL", true), v => {
-      Core.settings.put("BPL", true)
-      if (Core.settings.getBool("BPL")) {
-        Core.settings.put("BPR", false)
-        Core.settings.put("BPB", false)
-        Core.settings.put("BPT", false)
-      }
-    }).left().row()
+        if(v){
+            Core.settings.put("BPT", false)
+            Core.settings.put("BPR", false)
+            Core.settings.put("BPL", false)
+        }
+    }).left().center().row()
+    
+    t.check("Left", Core.settings.getBool("BPL", false), v => {
+        Core.settings.put("BPL", v)
+    
+        if(v){
+            Core.settings.put("BPT", false)
+            Core.settings.put("BPR", false)
+            Core.settings.put("BPB", false)
+        }
+    }).left().center().row()
     
   }))
   
