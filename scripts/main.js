bfd13215bef27b5e67a3c8e80f272868b9ec37d9
7bebebe7 <oleh.sobchenko@gmail.com>
2026-09-03 10:38:28 +0300

    button test

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,29 @@
 Events.on(ClientLoadEvent, e => {
   
+  var buttonActive = true
+  
   //добавление вкладки в настройках, и самих настроек (см. ниже)
   Vars.ui.settings.addCategory("Camera Point", Icon.file, cons(t => {
     //настройка показа кнопки 
     t.check("Show Camera Button", Core.settings.getBool("SCB", true), v => {
       Core.settings.put("SCB", v)
     }).left().center().row()
-    
-    //настройка позиции кнопки для удобной игры
-    t.add("[gray]Button Position").row()
-    
-    t.check("Right", Core.settings.getBool("BPR", false), v => {
-        Core.settings.put("BPR", v)
-    
-        if(v){
-            Core.settings.put("BPT", false)
-            Core.settings.put("BPB", false)
-            Core.settings.put("BPL", false)
-        }
-    }).left().center().row()
-    
-    t.check("Bottom", Core.settings.getBool("BPB", true), v => {
-        Core.settings.put("BPB", v)
-    
-        if(v){
-            Core.settings.put("BPT", false)
-            Core.settings.put("BPR", false)
-            Core.settings.put("BPL", false)
-        }
-    }).left().center().row()
-    
-    t.check("Left", Core.settings.getBool("BPL", false), v => {
-        Core.settings.put("BPL", v)
-    
-        if(v){
-            Core.settings.put("BPT", false)
-            Core.settings.put("BPR", false)
-            Core.settings.put("BPB", false)
-        }
-    }).left().center().row()
-    
   }))
   
   //добавление кнопки на екран при включенной настройке
-  
+    if (Core.settings.getBool("SCB", true)) {
+      
+      Vars.ui.hudGroup.fill(cons(t => {
+        
+        var but = t.button("Camera", () => {
+          Log.info("input B")
+        }).size(150, 60).center().top().pad(20).get()
+      
+      //чтобы взаемодействовать с кнопкой в переменной нада добавить .get() для кнопки
+      //взаемодействуем с кнопкой в переменной
+      //color взаемойствует с цветовыми каналами, "a" ето alpha, прозрачнось
+      but.color.a = 0.5
+      
+      }))
+    }
 })
